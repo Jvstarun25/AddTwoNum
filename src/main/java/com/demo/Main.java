131f8557aa4f3ed44a8f46a3a2da6ec126e4dfe3
Jvstarun25 <jvstarun1997@gmail.com>
2023-06-24 15:11:43 -0500

    This is a AddFunctionality

---
+++ src/main/java/com/demo/Main.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+
+}
